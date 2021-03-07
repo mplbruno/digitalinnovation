5a196293fa5b2908bfe180fffe42f100c17a6329
mplbruno <mplbruno@gmail.com>
2021-03-07 20:33:25 -0300

    Created pessoa class

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "marcos"
+    var cpf: String = "123.123.123-11"
+}
+
+fun main() {
+    val marcos = Pessoa()
+    println(marcos.nome)
+    println(marcos.cpf)
+}
+
+
+
